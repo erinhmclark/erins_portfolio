6c8088061ae0b1da87bfaa505f45557a718ec0ee
erinhmclark <erinhannahmary.clark@gmail.com>
2023-07-15 15:47:21 +0100

    Add tag model.

--- projects/models.py
+++ projects/models.py
@@ -2,12 +2,20 @@ from django.db import models
 from PIL import Image
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
 class Project(models.Model):
     title = models.CharField(max_length=200)
     external_link = models.URLField(max_length=200)
     summary = models.CharField(max_length=200)
     description = models.TextField()
     image = models.ImageField(default='default.jpeg', upload_to='project_images')
+    tags = models.ManyToManyField(Tag, null=True)
 
     def __str__(self):
         return self.title
